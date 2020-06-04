1d20a38a8191c0845f6f2d345f0664d7a86f7d09
Nissan Ahmed <aan007bd@gmail.com>
2020-06-04 17:55:02 +0000

    Update test

--- dFi.js
+++ dFi.js
@@ -1,19 +1,6 @@
 const { dFi, deezerApi } = require("d-fi");
 
-require("fs").readFile("~/.config/d-fi-nodejs/config.json", "utf8", (error, content) => {
-  let dfiArl;
-
-  if (error) console.error(error);
-  else {
-    try {
-      dfiArl = JSON.parse(content).cookies.arl;
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  deezerApi.arl = dfiArl || process.env.ARL;
-});
+deezerApi.arl = process.env.ARL;
 
 module.exports = async (trackUrl) => { 
   try {
--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 const downloader = require("./downloader");
 
-console.log(downloader.downloadMusic("https://open.spotify.com/track/6c5YmpkzmCBjCuerCPU6ng?si=1rsX7hRSTgCU2NKGYzA_Ag"));
+const paths = downloader.downloadMusic("https://open.spotify.com/track/6c5YmpkzmCBjCuerCPU6ng?si=1rsX7hRSTgCU2NKGYzA_Ag");
+
+console.log(paths);
