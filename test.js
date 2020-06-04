12d7fbec3d68bbefaa99e606c43dc4639dba27b8
Nissan Ahmed <aan007bd@gmail.com>
2020-06-04 18:02:44 +0000

    Update test with proper Promise handling

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
 const downloader = require("./downloader");
 
-const paths = downloader.downloadMusic("https://open.spotify.com/track/6c5YmpkzmCBjCuerCPU6ng?si=1rsX7hRSTgCU2NKGYzA_Ag");
-
-console.log(paths);
+downloader.downloadMusic("https://open.spotify.com/track/6c5YmpkzmCBjCuerCPU6ng?si=1rsX7hRSTgCU2NKGYzA_Ag").then((paths) => { console.log(paths) });
