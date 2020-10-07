42375296dad1ccae9f35eecca3a60be4c9051d18
Nissan Ahmed <aan007bd@gmail.com>
2020-10-07 09:01:34 +0600

    Replace IFTTT notifier with a generic webhook URL

--- src/scripts/notifier.js
+++ src/scripts/notifier.js
@@ -2,22 +2,20 @@ const { name: programName } = require("../../package.json");
 const fetch = require("isomorphic-fetch");
 
 /**
- * Trigger a "/push" webhook on IFTTT.
- * Can be tested directly on "Documentation" page of https://ifttt.com/maker_webhooks
+ * Triggers a webhook with { title: string, body: string }.
  * 
  * @param {string} message 
  * @param {Error} error 
  */
 exports.push = async (message, error) => {
   const body = {
-    value1: programName,
-    value2: message,
-    value3: `${error.message}\n${error.stack}`,
+    title: `[${programName}] ${message}`,
+    body: `${error.message}\n${error.stack}`,
   };
 
   try {
     await fetch(
-      `https://maker.ifttt.com/trigger/push/with/key/${process.env.IFTTT_WEBHOOK_KEY}`,
+      process.env.NOTIFIER_WEBHOOK_URL,
       {
         method: "post",
         body: JSON.stringify(body),
